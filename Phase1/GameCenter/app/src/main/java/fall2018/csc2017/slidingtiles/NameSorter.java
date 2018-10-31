6f8eff282d7f61b70e09b384b8174aab79eb61b5
Chenjie Zhao <johnson.zhao@mail.utoronto.ca>
2018-10-30 21:33:14 -0400

    Johnson Changed NameSorter

--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/NameSorter.java
+++ Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/NameSorter.java
@@ -1,15 +1,19 @@
 package fall2018.csc2017.slidingtiles;
-import java.util.ArrayList;
-import java.lang.String;
 
-public class NameSorter implements Sorter{
-    public void sort(ArrayList<Object[]> nameAndScore){
+import android.util.Pair;
+import java.lang.String;
+import java.util.List;
+
+public class NameSorter implements Sorter<String, Integer>{
+
+    @Override
+    public void sort(List<Pair<String, Integer>> nameAndScore){
         for (int i = (nameAndScore.size() - 1); i >= 0; i--){
             for (int j = 1; j <= i; j++){
-                String a = (String) nameAndScore.get(j - 1)[0];
-                String b = (String) nameAndScore.get(j)[0];
+                String a = (String) nameAndScore.get(j - 1).first;
+                String b = (String) nameAndScore.get(j).first;
                 if (a.compareTo(b) < 0){
-                    Object[] temp = nameAndScore.get(j - 1);
+                    Pair temp = nameAndScore.get(j - 1);
                     nameAndScore.set(j - 1, nameAndScore.get(j));
                     nameAndScore.set(j, temp);
                 }
