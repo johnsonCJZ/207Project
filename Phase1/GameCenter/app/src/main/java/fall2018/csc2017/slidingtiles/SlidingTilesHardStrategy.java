0d6e5d6ab204f3136671866f8776ca002b7c61ef
summer <summer.tao@mail.utoronto.ca>
2018-10-30 21:29:20 -0400

    medium, hard strategy deleted

--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingTilesHardStrategy.java
+++
@@ -1,8 +0,0 @@
-package fall2018.csc2017.slidingtiles;
-
-public class SlidingTilesHardStrategy implements Strategy {
-    @Override
-    public int calculateScore() {
-        return steps * 7.77 - undosteps ;
-    }
-}
--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingTilesMediumStrategy.java
+++
@@ -1,9 +0,0 @@
-package fall2018.csc2017.slidingtiles;
-
-public class SlidingTilesMediumStrategy implements Strategy {
-    @Override
-    public int calculateScore() {
-        if (time < ){
-        return step * 6.66 + ;
-    }
-}
