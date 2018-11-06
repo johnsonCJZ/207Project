1cb4db5fcf00a980db6bce996dc6680873d6b07d
FrankChen <frankchen0717@gmail.com>
2018-11-06 00:57:17 -0500

    Frank: fixed Javadoc and scoreBoard

--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/ScoreBoard.java
+++ Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/ScoreBoard.java
@@ -4,22 +4,48 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The scoreBoard to display rankings.
+ */
 class ScoreBoard implements Serializable {
     private int scoreBoardSize = 10;
-    private List<Object[]> scoreList;
+    private List<Object[]> scoreList = new ArrayList<>();
     private SlidingTilesScoreStrategy scoreStrategy = new SlidingTilesScoreStrategy();
+    private ScoreSorter scoreSorter = new ScoreSorter();
 
-    ScoreBoard() {
-        this.scoreList = new ArrayList<>(scoreBoardSize + 1);
+    /**
+     * Set the scoreBoardSize to size.
+     * @param size the new size to set
+     */
+    void setScoreBoardSize(int size) {
+        scoreBoardSize = size;
     }
 
     int calculateScore(BoardManager boardManager) {
         return scoreStrategy.calculateScore(boardManager);
     }
 
-    void addAndSort(Object[] scoreArray) {
-        ScoreSorter scoreSorter = new ScoreSorter();
+    /**
+     * Set the scoreStrategy to strategy. Open to modification.
+     * @param strategy the ScoreStrategy to set
+     */
+    void setScoreStrategy(SlidingTilesScoreStrategy strategy) {
+        scoreStrategy = strategy;
+    }
 
+    /**
+     * Set the scoreSorter to sorter. Open to modification.
+     * @param sorter the Sorter to set
+     */
+    void setScoreSorter(ScoreSorter sorter) {
+        scoreSorter = sorter;
+    }
+
+    /**
+     * Add a new result to the scoreList and sort the list to get the new ranking.
+     * @param scoreArray the new result to add
+     */
+    void addAndSort(Object[] scoreArray) {
         scoreList.add(scoreArray);
         scoreSorter.sort(scoreList);
         if (scoreList.size() == scoreBoardSize + 1) {
@@ -27,6 +53,10 @@ class ScoreBoard implements Serializable {
         }
     }
 
+    /**
+     * Return the scoreList.
+     * @return the scoreList
+     */
     List<Object[]> getScoreList() {
         return scoreList;
     }
--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/Strategy.java
+++
@@ -1,5 +0,0 @@
-package fall2018.csc2017.slidingtiles;
-
-public interface Strategy<T> {
-    public int calculateScore(T o);
-}
