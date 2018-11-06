4de1de9945cf4dcb3cf5c1b77c7122eb45259c0c
FrankChen <frankchen0717@gmail.com>
2018-11-05 19:50:53 -0500

    Frank: updated SlidingTilesScoreStrategy

--- Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingTilesScoreStrategy.java
+++ Phase1/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/SlidingTilesScoreStrategy.java
@@ -2,14 +2,47 @@ package fall2018.csc2017.slidingtiles;
 
 import java.io.Serializable;
 
+/**
+ *
+ */
 public class SlidingTilesScoreStrategy implements Strategy<BoardManager>, Serializable {
 
-    @Override
-    public int calculateScore(BoardManager boardManager) {
-        double time = boardManager.getTime();
-        int step = boardManager.getHistory().getSize();
+    /**
+     *
+     * @param boardManager
+     * @return
+     */
+    private int calStepScore(BoardManager boardManager) {
+        int step = boardManager.getHistory().getSize()-1;
 
-        return (int) Math.round(10000/time + 10000/step);
-        //should also consider time
+        return Math.round(100 - step/5);
+    }
+
+    /**
+     *
+     * @param boardManager
+     * @return
+     */
+    private int calTimeScore(BoardManager boardManager) {
+        double time = boardManager.getTime();
+
+        return (int) Math.round((100 - time/20));
+
+    }
+
+    @Override
+    /**
+     *
+     * @param boardManager
+     * @return the score calculated
+     */
+    public int calculateScore(BoardManager boardManager) {
+        int stepScore = calStepScore(boardManager);
+        int timeScore = calTimeScore(boardManager);
+
+        if (stepScore < 0) {stepScore = 0;}
+        if (timeScore < 0) {timeScore = 0;}
+
+        return  (stepScore + timeScore)/2;
     }
 }
