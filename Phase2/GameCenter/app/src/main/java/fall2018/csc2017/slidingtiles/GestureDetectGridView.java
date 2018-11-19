f630ed6ff6d10566b323c1d4e09245db803c8802
HP <haoyanhy.jiang@utoronto.ca>
2018-11-19 14:22:28 -0500

    Max, debuging 2048,  gesture detector intact, merge logic is wrong, wait for johnson

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/GestureDetectGridView.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/GestureDetectGridView.java
@@ -15,6 +15,9 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class GestureDetectGridView extends GridView {
     public static final int SWIPE_MIN_DISTANCE = 100;
@@ -83,17 +86,21 @@ public class GestureDetectGridView extends GridView {
                     if (Math.abs(diffX) > Math.abs(diffY)) {
                         if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                             if (diffX > 0) {
-                                mController.processLeftSwipe();
-                            } else {
+                                Toasty.success(context, "Right", Toast.LENGTH_SHORT, true).show();
                                 mController.processRightSwipe();
+                            } else {
+                                Toasty.success(context, "Left", Toast.LENGTH_SHORT, true).show();
+                                mController.processLeftSwipe();
                             }
                             result = true;
                         }
                     }
                     else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
                         if (diffY > 0) {
+                            Toasty.success(context, "Down", Toast.LENGTH_SHORT, true).show();
                             mController.processDownSwipe();
                         } else {
+                            Toasty.success(context, "Up", Toast.LENGTH_SHORT, true).show();
                             mController.processUpSwipe();
                         }
                         result = true;
