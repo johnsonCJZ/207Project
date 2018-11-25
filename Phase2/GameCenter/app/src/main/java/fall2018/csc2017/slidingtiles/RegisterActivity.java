bc7bebe12cb4cf4c59eb3026abc14407acd40d1f
taoyi3 <1cptbtptp1A>
2018-11-24 19:06:15 -0500

    regex added and tested

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/RegisterActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/RegisterActivity.java
@@ -16,6 +16,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import es.dmoral.toasty.Toasty;
 import fall2018.csc2017.slidingtiles.database.DatabaseHelper;
@@ -30,6 +32,11 @@ public class RegisterActivity extends AppCompatActivity {
     Button bRegister;
     UserAccountManager userAccountManager;
     UserAccount newUser;
+    String usernameS;
+    String passwordS;
+    String confirmPWS;
+    String emailS;
+    String ageS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +46,12 @@ public class RegisterActivity extends AppCompatActivity {
             userAccountManager = new UserAccountManager();
         }
         setContentView(R.layout.activity_register);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        confirm = findViewById(R.id.confirmpw);
-        email = findViewById(R.id.email);
-        age = findViewById(R.id.age);
-        bRegister = findViewById(R.id.bRegister);
+        updateInfo();
 
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                updateInfo();
                 if(registerButtonPushed()) {
                     boolean update = myDB.createAndInsertNew(newUser.getName(), myDB.convertToJson(newUser));
                     if (update) {
@@ -63,42 +66,73 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void updateInfo(){
+        username = findViewById(R.id.username);
+        usernameS = this.username.getText().toString();
+        password = findViewById(R.id.password);
+        passwordS = this.password.getText().toString();
+        confirm = findViewById(R.id.confirmpw);
+        confirmPWS = confirm.getText().toString();
+        email = findViewById(R.id.email);
+        emailS = email.getText().toString();
+        age = findViewById(R.id.age);
+        ageS = age.getText().toString();
+        bRegister = findViewById(R.id.bRegister);
+    }
+
     /**
      * Return true when a new account is registered, return false when a username is taken or
      * the confirming password does not match.
      * @return a boolean showing whether an account is successfully registered
      */
     private boolean registerButtonPushed() {
-        String username = this.username.getText().toString();
-        String password = this.password.getText().toString();
-        String confirmPW = confirm.getText().toString();
-        String emails = email.getText().toString();
         ArrayList<String> userList = userAccountManager.getUserList();
-        newUser = new UserAccount(username, password);
-        TextView textView = findViewById(R.id.textView7);
-        if(!(age.getText().toString().equals(""))){
-            Integer agei = Integer.parseInt(age.getText().toString());
-            newUser.setAge(agei);
+        newUser = new UserAccount(usernameS, passwordS);
+        TextView message = findViewById(R.id.message);
+        if(!(ageS.equals(""))){
+            Integer ageI = Integer.parseInt(ageS);
+            newUser.setAge(ageI);
         }
         if (!email.equals("")){
-            newUser.setEmail(emails);
+            newUser.setEmail(emailS);
         }
         if (!userList.isEmpty()){
             for (String account : userList) {
                 if (account.equals(username)) {
-                    textView.setText("this name is taken");
+                    message.setText("this name is taken.");
                     return false;
                 }
-                //tell the user the username already exists.
-        }}
-        if (!password.equals(confirmPW)) {
-            textView.setText("password doesn't match");
+            }}
+        if (!passwordS.equals(confirmPWS)) {
+            message.setText("password doesn't match.");
             return false;
         }
-//        Tell the user the password and confirmed password are not the same
+        if(!validateInfo(usernameS, "^[a-z]{3,7}$")){
+            message.setText("Illegal input of username.");
+            return false;
+        }
+        if(!(validateInfo(passwordS, "^[a-z0-9]{1,9}$"))){
+            message.setText("Illegal input of password.");
+            return false;
+        }
+        if(!validateInfo(emailS, "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")){
+            message.setText("Illegal input of  email address.");
+            return false;
+        }
+        if(!validateInfo(ageS,"^[1-9][0-9]?$")){
+            message.setText("Illegal input of age.");
+            return false;
+        }
         userAccountManager.addUser(newUser.getName());
         saveToFile(UserAccountManager.USERS);
         return true;
+    }
+
+    private boolean validateInfo(String info, String regex){
+        String infoRegex = regex;
+        Pattern regexP = Pattern.compile(infoRegex);
+        Matcher matcher = regexP.matcher(info);
+        return matcher.matches();
     }
 
     /**
