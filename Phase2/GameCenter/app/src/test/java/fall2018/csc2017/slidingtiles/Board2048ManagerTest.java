4c4548234add731c146f64cbbe1b45ae6daab9f9
zhangxinyi <charliexinyi371@gmail.com>
2018-11-27 14:31:27 -0500

    Xinyi - Revised board2048 tests.

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/Board2048ManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/Board2048ManagerTest.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.*;
 
 public class Board2048ManagerTest {
 
-    private void setBoard(int largestTile, Board2048 board){
+    private void setBoard(int largestTile, Game2048Board board){
         int n = 2;
         for (int i = 0; i < 4; i++){
             for (int j = 0; j < 4; j++){
-                Tile2048 tile = board.getTiles()[i][j];
+                Game2048Tile tile = board.getTiles()[i][j];
                 tile.setValue(n);
                 if(n < largestTile){
                     n = n * 2;
@@ -25,48 +25,48 @@ public class Board2048ManagerTest {
 
     @Test
     public void testLose() {
-        Board2048Manager manager = new Board2048Manager();
-        Board2048 board = manager.getBoard();
+        Game2048BoardManager manager = new Game2048BoardManager();
+        Game2048Board board = manager.getBoard();
         setBoard(1024, board);
         assertTrue(manager.isLose());
     }
 
     @Test
     public void testNotLoseHas2048CanMove() {
-        Board2048Manager manager = new Board2048Manager();
+        Game2048BoardManager manager = new Game2048BoardManager();
         manager.cheat();
         assertFalse(manager.isLose());
     }
 
     @Test
     public void testNotLoseHas2048CantMove() {
-        Board2048Manager manager = new Board2048Manager();
-        Board2048 board = manager.getBoard();
+        Game2048BoardManager manager = new Game2048BoardManager();
+        Game2048Board board = manager.getBoard();
         setBoard(2048, board);
         assertFalse(manager.isLose());
     }
 
     @Test
     public void testWinContains2048CantMove() {
-        Board2048Manager manager = new Board2048Manager();
-        Board2048 board = manager.getBoard();
+        Game2048BoardManager manager = new Game2048BoardManager();
+        Game2048Board board = manager.getBoard();
         setBoard(2048, board);
         assertTrue(manager.isWon());
     }
 
     @Test
     public void testNotWinCanMove() {
-        Board2048Manager manager = new Board2048Manager();
-        Board2048 board = manager.getBoard();
-        Tile2048 tile= board.getTile(0, 0);
+        Game2048BoardManager manager = new Game2048BoardManager();
+        Game2048Board board = manager.getBoard();
+        Game2048Tile tile= board.getTile(0, 0);
         tile.setValue(2);
         assertFalse(manager.isWon());
     }
 
     @Test
     public void testNotWinCantMove() {
-        Board2048Manager manager = new Board2048Manager();
-        Board2048 board = manager.getBoard();
+        Game2048BoardManager manager = new Game2048BoardManager();
+        Game2048Board board = manager.getBoard();
         setBoard(1024, board);
         assertFalse(manager.isWon());
     }
--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/Board2048Test.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/Board2048Test.java
@@ -6,197 +6,317 @@ import static org.junit.Assert.*;
 
 public class Board2048Test {
 
+    private int[] lineToInt(Game2048Tile[] line) {
+        int[] intLine = new int[4];
+        for (int i = 0; i < 4; i++) {
+            intLine[i] = line[i].getValue();
+        }
+        return intLine;
+    }
+
     @Test
     public void simpleSlideRight() {
         int[] expected = {0, 0, 2, 4};
-        Tile2048[] before = {
-                new Tile2048(2), new Tile2048(4), new Tile2048(0), new Tile2048(0),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(2);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(4);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(0);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void simpleMergeRight() {
         int[] expected = {0, 0, 0, 4};
-        Tile2048[] before = {
-                new Tile2048(0), new Tile2048(0), new Tile2048(2), new Tile2048(2),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(0);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(0);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(2);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void slideAndMergeRight() {
         int[] expected = {0, 0, 0, 8};
-        Tile2048[] before = {
-                new Tile2048(4), new Tile2048(0), new Tile2048(4), new Tile2048(0),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(4);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(4);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(0);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
 
     }
 
     @Test
     public void doNothingRight() {
         int[] expected = {0, 0, 2, 4};
-        Tile2048[] before = {
-                new Tile2048(0), new Tile2048(0), new Tile2048(2), new Tile2048(4),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(0);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(0);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(4);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void moreTilesRight() {
         int[] expected = {0, 0, 4, 4};
-        Tile2048[] before = {
-                new Tile2048(0), new Tile2048(2), new Tile2048(2), new Tile2048(4),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(0);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(0);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(4);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void largerNumbersRight() {
         int[] expected = {0, 0, 2048, 512};
-        Tile2048[] before = {
-                new Tile2048(1024), new Tile2048(0), new Tile2048(1024), new Tile2048(512),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(1024);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(0);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(1024);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(512);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void noZeroesRight() {
         int[] expected = {0, 512, 2048, 512};
-        Tile2048[] before = {
-                new Tile2048(256), new Tile2048(256), new Tile2048(2048), new Tile2048(512),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(256);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(256);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2048);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(512);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "RIGHT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
+    }
+
+    @Test
+    public void moreNumbersTwoMergesRight() {
+        int[] expected = {0, 0, 128, 256};
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(64);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(64);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(128);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(128);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
+        };
+        Game2048Board board = new Game2048Board();
+        board.mergeList(before, "RIGHT");
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void simpleSlideLeft() {
         int[] expected = {2, 4, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(0), new Tile2048(0), new Tile2048(2), new Tile2048(4),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(0);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(0);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(4);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void simpleMergeLeft() {
         int[] expected = {4, 0, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(2), new Tile2048(2), new Tile2048(0), new Tile2048(0),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(2);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(2);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(0);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void slideAndMergeLeft() {
         int[] expected = {8, 0, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(0), new Tile2048(4), new Tile2048(0), new Tile2048(4),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(0);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(4);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(4);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
 
     }
 
     @Test
     public void doNothingLeft() {
         int[] expected = {1024, 2, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(1024), new Tile2048(2), new Tile2048(0), new Tile2048(0),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(1024);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(2);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(0);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void moreTilesLeft() {
-        int[] expected = {4, 2, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(4), new Tile2048(2), new Tile2048(2), new Tile2048(0),
+        int[] expected = {4, 4, 0, 0};
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(4);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(2);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(2);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(0);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void largerNumbersLeft() {
         int[] expected = {512, 2048, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(512), new Tile2048(1024), new Tile2048(0), new Tile2048(1024),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(512);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(1024);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(0);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(1024);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void noZeroesLeft() {
         int[] expected = {512, 2048, 512, 0};
-        Tile2048[] before = {
-                new Tile2048(512), new Tile2048(2048), new Tile2048(256), new Tile2048(256),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(512);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(2048);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(256);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(256);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 
     @Test
     public void moreNumbersTwoMergesLeft() {
         int[] expected = {16, 32, 0, 0};
-        Tile2048[] before = {
-                new Tile2048(8), new Tile2048(8), new Tile2048(16), new Tile2048(16),
+        Game2048Tile tile1 = new Game2048Tile();
+        tile1.setValue(8);
+        Game2048Tile tile2 = new Game2048Tile();
+        tile2.setValue(8);
+        Game2048Tile tile3 = new Game2048Tile();
+        tile3.setValue(16);
+        Game2048Tile tile4 = new Game2048Tile();
+        tile4.setValue(16);
+        Game2048Tile[] before = {
+                tile1, tile2, tile3, tile4
         };
-        Board2048 board = new Board2048();
+        Game2048Board board = new Game2048Board();
         board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
-    }
-
-    @Test
-    public void moreNumbersTwoMergesRight() {
-        int[] expected = {0, 0, 128, 256};
-        Tile2048[] before = {
-                new Tile2048(64), new Tile2048(64), new Tile2048(128), new Tile2048(128),
-        };
-        Board2048 board = new Board2048();
-        board.mergeList(before, "LEFT");
-        Converter conv = new Converter();
-        assertEquals(expected, conv.lineToInt(before));
+        assertEquals(expected, lineToInt(before));
     }
 }
