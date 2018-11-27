4936a20f444b6e9b26d3c873302e52919e2d2f20
zhangxinyi <charliexinyi371@gmail.com>
2018-11-26 23:07:44 -0500

    Xinyi - Board2048Manager unit test.

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingBoard2048GameManagerTestBoard.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/SlidingBoard2048GameManagerTestBoard.java
@@ -5,45 +5,69 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class SlidingBoard2048GameManagerTestBoard {
+public class Board2048ManagerTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private void setBoard(int largestTile, Board2048 board){
+        int n = 2;
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                Tile2048 tile = board.getTiles()[i][j];
+                tile.setValue(n);
+                if(n < largestTile){
+                    n = n * 2;
+                }
+                else{
+                    n = 2;
+                }
+            }
+        }
     }
 
     @Test
-    public void getBoard() {
+    public void testLose() {
+        Board2048Manager manager = new Board2048Manager();
+        Board2048 board = manager.getBoard();
+        setBoard(1024, board);
+        assertTrue(manager.isLose());
     }
 
     @Test
-    public void cheat() {
+    public void testNotLoseHas2048CanMove() {
+        Board2048Manager manager = new Board2048Manager();
+        manager.cheat();
+        assertFalse(manager.isLose());
     }
 
     @Test
-    public void setTiles() {
+    public void testNotLoseHas2048CantMove() {
+        Board2048Manager manager = new Board2048Manager();
+        Board2048 board = manager.getBoard();
+        setBoard(2048, board);
+        assertFalse(manager.isLose());
     }
 
     @Test
-    public void isLose() {
+    public void testWinContains2048CantMove() {
+        Board2048Manager manager = new Board2048Manager();
+        Board2048 board = manager.getBoard();
+        setBoard(2048, board);
+        assertTrue(manager.isWon());
     }
 
     @Test
-    public void isWin() {
+    public void testNotWinCanMove() {
+        Board2048Manager manager = new Board2048Manager();
+        Board2048 board = manager.getBoard();
+        Tile2048 tile= board.getTile(0, 0);
+        tile.setValue(2);
+        assertFalse(manager.isWon());
     }
 
     @Test
-    public void moveUp() {
-    }
-
-    @Test
-    public void moveDown() {
-    }
-
-    @Test
-    public void moveLeft() {
-    }
-
-    @Test
-    public void moveRight() {
+    public void testNotWinCantMove() {
+        Board2048Manager manager = new Board2048Manager();
+        Board2048 board = manager.getBoard();
+        setBoard(1024, board);
+        assertFalse(manager.isWon());
     }
 }
