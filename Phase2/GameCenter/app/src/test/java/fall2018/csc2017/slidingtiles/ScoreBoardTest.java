de5e42cb4427ee493123c568a1af1395ab575d5b
FrankChen <frankchen0717@gmail.com>
2018-11-29 18:10:54 -0500

    Frank: add test file for scoreboard

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/ScoreBoardTest.java
@@ -0,0 +1,20 @@
+package fall2018.csc2017.slidingtiles;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreBoardTest {
+
+    @Test
+    public void calculateScore() {
+    }
+
+    @Test
+    public void addAndSort() {
+    }
+
+    @Test
+    public void getScoreList() {
+    }
+}
