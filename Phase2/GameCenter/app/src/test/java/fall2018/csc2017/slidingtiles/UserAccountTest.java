41ceb950c066e48ff92e762164a88dd4ec5e17d2
taoyi3 <1cptbtptp1A>
2018-11-30 07:13:26 -0500

    UserAccountTest edited. UserAccountManagerTest added.

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/UserAccountTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/UserAccountTest.java
@@ -12,27 +12,17 @@ public class UserAccountTest {
     }
 
     @Test
-    public void getHistorySliding() {
+    public void testCreateNewUser() {
         Factory f = new Factory();
         UserAccount newUser = f.createUserAccount("aaa", "b");
         assertEquals("aaa",newUser.getName());
         assertEquals("b",newUser.getPassword());
-
         newUser.setPassword("1a2c");
         assertEquals("1a2c",newUser.getPassword());
-
         newUser.setName("abcabc");
         assertEquals("abcabc",newUser.getName());
-
-        assertNull(newUser.getSpecific2048History("resumeHistory2048"));
-        assertNull(newUser.getSpecificSlideHistory("history3x3"));
-        assertNull(newUser.getSpecificSlideHistory("history4x4"));
-        assertNull(newUser.getSpecificSlideHistory("history5x5"));
-        assertNull(newUser.getSpecificMineHistory("historyMine"));
-
         newUser.setEmail("abc@gmail.com");
         assertEquals("abc@gmail.com",newUser.getEmail());
-
         newUser.setAge(3);
         assertEquals(3,(int) newUser.getAge());
     }
