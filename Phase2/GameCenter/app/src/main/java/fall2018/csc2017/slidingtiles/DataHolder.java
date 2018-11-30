bf9afde5df54bd14fe6e5bf85f2c1386d9035fa8
HP <haoyanhy.jiang@utoronto.ca>
2018-11-30 10:01:02 -0500

    Max, javadoc

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/DataHolder.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/DataHolder.java
@@ -5,17 +5,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DataHolder {
+    /**
+     * DataHolder saves current user in system
+     */
     private static final DataHolder holder = new DataHolder();
+
+    /**
+     * data holding data memory
+     */
     Map<String, WeakReference<Object>> data = new HashMap<String, WeakReference<Object>>();
 
+    /**
+     * get holder object
+     * @return object holding data
+     */
     public static DataHolder getInstance() {
         return holder;
     }
 
+    /**
+     * save data
+     * @param id data reference
+     * @param object data object
+     */
     public void save(String id, Object object) {
         data.put(id, new WeakReference<Object>(object));
     }
 
+    /**
+     * retrieve data from data object
+     * @param id data reference
+     * @return memory
+     */
     public Object retrieve(String id) {
         WeakReference<Object> objectWeakReference = data.get(id);
         return objectWeakReference.get();
